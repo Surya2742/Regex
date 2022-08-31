2b7a81dc80b3bfd5d3752bfa2468c95ad767928e
Surya2742 <boby7724284@gmail.com>
2022-08-31 19:33:08 +0530

    added: Functionality to accept LastName starts with Cap and min 3 char.

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -5,14 +5,20 @@ public class UserRegistration {
     public void firstNameValidation() {
         Scanner sc = new Scanner(System.in);
         final String FIRSTNAME = "([A-Z][a-z]{2,})";
-        System.out.print("Enter the FirstName to validate : ");
+        System.out.print("Enter the FirstName & LastName to validate\nFirstName : ");
         String firstName = sc.nextLine();
+        System.out.print("LastName : ");
+        String lastName = sc.nextLine();
         boolean resultForFirstName = Pattern.matches(FIRSTNAME, firstName);
-        if (resultForFirstName) {
-            System.out.println("First Name is valid and acceptable.");
+        boolean resultForLastName = Pattern.matches(FIRSTNAME, lastName);
+        if (resultForFirstName && resultForLastName) {
+            System.out.println("First Name & Last Name both are valid and acceptable.");
         }
-        else {
+        else if (!resultForFirstName){
             System.out.println("Enter valid First Name");
+            firstNameValidation();
+        } else if (!resultForLastName) {
+            System.out.println("Enter valid Last Name");
             firstNameValidation();
         }
     }
